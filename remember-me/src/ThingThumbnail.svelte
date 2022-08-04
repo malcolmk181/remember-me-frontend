298efdeb8ecd35e64b042417bb0db9b5c3c4f047
Malcolm Keyes <3.malcolmk@gmail.com>
2022-08-04 11:35:44 -0400

    documentation for thingThumbnail

--- remember-me/src/ThingThumbnail.svelte
+++ remember-me/src/ThingThumbnail.svelte
@@ -7,6 +7,7 @@
 
 	const dispatch = createEventDispatcher();
 
+	// Dispatch the id of a thing to App to load it into ThingDetails
 	function handleClick() {
 		dispatch('message', {
 			id: id,
@@ -14,6 +15,7 @@
 		});
 	}
 
+	// Dispatch the id of the child to ThingDetails for deletion of the relationship
 	function handleDelete() {
 		dispatch('delete', {
 			id: id
@@ -21,9 +23,11 @@
 	}
 </script>
 
+<!-- Thing thumbnail. Uses Bulma's "tag" element -->
 <span class="tag is-medium is-clickable" data-id={id} on:click="{handleClick}">
 	{name}
 	{#if deletable}
+		<!-- Delete button -->
 		<span class="icon is-small ml-3">
 			<i class="fas fa-times" on:click|stopPropagation="{handleDelete}"></i>
 		</span>
