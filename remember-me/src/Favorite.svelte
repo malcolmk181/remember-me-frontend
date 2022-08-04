07c7abf787073df407c3a35905a911f43f788ac6
Malcolm Keyes <3.malcolmk@gmail.com>
2022-08-04 11:38:22 -0400

    documentation for thingFavorite

--- remember-me/src/Favorite.svelte
+++ remember-me/src/Favorite.svelte
@@ -6,6 +6,7 @@
 
 	const dispatch = createEventDispatcher();
 
+    // Dispatch the id of a thing to App to load it into ThingDetails
 	function handleClick() {
 		dispatch('message', {
 			id: favorite.id
@@ -13,6 +14,8 @@
 	}
 </script>
 
+<!-- An item in the favorites panel. Uses Bulma's "panel" element. -->
+<!-- The 'is-active' class changes the icon's color to match the color of the panel. -->
 <!-- svelte-ignore a11y-missing-attribute -->
 <a class="panel-block {(thing && thing.data.id === favorite.id) ? 'is-active' : ''}" data-id='{favorite.id}' on:click="{handleClick}">
     <span class="panel-icon">
